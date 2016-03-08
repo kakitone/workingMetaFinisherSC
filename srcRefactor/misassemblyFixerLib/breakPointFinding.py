011ccc23713030f0d091fc0b12e8515ead8c0adb
kakitone <kakitone@gmail.com>
2016-03-08 10:20:46 -0800

    debug

--- srcRefactor/misassemblyFixerLib/breakPointFinding.py
+++ srcRefactor/misassemblyFixerLib/breakPointFinding.py
@@ -250,16 +250,16 @@ def addHiddenBkPts(bkpts):
 		toAddPts = toAddPtsFormat(toAddPts, len(bkpts))
 		newBkpts =  toAddPts + bkpts
 
-		bkpts = filterDuplicate(newBkpts)
-		#bkpts = newBkpts
+		#bkpts = filterDuplicate(newBkpts)
+		bkpts = newBkpts
 
 		
 	return bkpts
 
 def clusterBkPts(oldbkts):	
 	### Get merge pairs 
-	newbkts = oldbkts
-	#newbkts = filterDuplicate(oldbkts)
+	#newbkts = oldbkts
+	newbkts = filterDuplicate(oldbkts)
 	newbkts.sort(key = itemgetter(-2, -1))
 	mergingPair = []
 	basicItems = []
